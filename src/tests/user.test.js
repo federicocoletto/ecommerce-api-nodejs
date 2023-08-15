5ef2751e9d64f18db6e8144acf3c9f2161ce03ac
federicocoletto <federico.coletto@fce.uncu.edu.ar>
2023-08-14 23:58:15 -0300

    + npm jest supertest + index.js (models) + testMigrate.js

---
+++ src/tests/user.test.js
@@ -0,0 +1,15 @@
+// const request = require("supertest");
+// const app = require("../app");
+
+// const user = {
+//     firstName: "Fede",
+//     lastName: "Coletto",
+//     email: "federico.coletto@fce.uncu.edu.ar",
+//     password: "1234",
+//     phone: "2612539374"
+// };
+// let userId;
+
+// test("POST --> '/api/v1/users' should return statusCode 201 and res.body.firstName === user.firstName", async () => {
+// 	const res = 
+// })
