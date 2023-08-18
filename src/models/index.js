13541ca965a90e7ab7a6ccb32793631ca2c4f1d6
federicocoletto <federico.coletto@fce.uncu.edu.ar>
2023-08-17 22:45:30 -0300

    cart get, remove

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,16 @@
+const Cart = require("./Cart");
 const Category = require("./Category");
 const Product = require("./Product");
 const User = require("./User");
 
 // Product --> categoryId (fk)
-Product.belongsTo(Category) 
-Category.hasMany(Product)
+Product.belongsTo(Category);
+Category.hasMany(Product);
+
+// Cart --> userId (fk)
+Cart.belongsTo(User);
+User.hasOne(Cart);
+
+// Cart --> productId (fk)
+Cart.belongsTo(Product);
+Product.hasMany(Cart);
