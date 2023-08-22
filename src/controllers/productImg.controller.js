ca62d438398a7012b96903461c3975a3b803cfd3
federicocoletto <federico.coletto@fce.uncu.edu.ar>
2023-08-21 23:35:24 -0300

    + productImg remove

--- src/controllers/productImg.controller.js
+++ src/controllers/productImg.controller.js
@@ -1,5 +1,7 @@
 const catchError = require("../utils/catchError");
 const ProductImg = require("../models/ProductImg");
+const fs = require("fs");
+const path = require("path");
 
 const getAll = catchError(async (req, res) => {
 	const results = await ProductImg.findAll();
@@ -8,12 +10,22 @@ const getAll = catchError(async (req, res) => {
 
 const create = catchError(async (req, res) => {
 	const { filename } = req.file;
-	
-	return res.sendStatus(201)
-	
+	const url = `${req.protocol}://${req.headers.host}/uploads/${filename}`;
+	const result = await ProductImg.create({ filename, url });
+	return res.status(201).json(result);
+});
+
+const remove = catchError(async (req, res) => {
+	const {id} = req.params
+	const result = await ProductImg.findByPk(id)
+	if(!result) return res.sendStatus(404)
+	fs.unlinkSync(path.join(__dirname, '..', 'public', 'uploads', `${result.filename}`))
+	await result.destroy()
+	return res.sendStatus(204);
 });
 
 module.exports = {
 	getAll,
 	create,
+	remove
 };
--- src/routes/productImg.router.js
+++ src/routes/productImg.router.js
@@ -1,11 +1,19 @@
-const { getAll, create } = require("../controllers/productImg.controller");
-const express = require('express');
+const {
+	getAll,
+	create,
+	remove,
+} = require("../controllers/productImg.controller");
+const express = require("express");
 const upload = require("../utils/multer");
 
 const routerProductImg = express.Router();
 
-routerProductImg.route('/')
-	.get(getAll)
-	.post(upload.single('image'), create)
+routerProductImg
+	.route("/")
+	.get(getAll) //
+	.post(upload.single("image"), create);
 
+routerProductImg
+	.route("/:id") //
+	.delete(remove);
 module.exports = routerProductImg;
